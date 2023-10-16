14107adab89a4bb15cf62f8eb9464b7a0877d53d
Ritwik28 <itsritwik26@gmail.com>
2023-10-16 13:36:27 +0530

    add custom lint rules

--- lib/api.dart
+++ lib/api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-import 'Album.dart';
+import 'album.dart';
 
 class SpotifyAPI {
   //Initialise necessary variables from Spotify API
